7a77f76cc59199fc739db608bf116691ad780525
Mihir Davada <101976200+MihirDavada@users.noreply.github.com>
2022-06-22 22:12:09 +0530

    Add files via upload

---
+++ DAY3/Code/day3.py
@@ -0,0 +1,132 @@
+# n1 = int(input('Enter The First Number'))
+# n2 = int(input('Enter The Second Number'))
+
+# ans = n1 + n2
+
+# print(ans)
+
+# x = 10
+# y = 6
+
+# print(f" x + y = {x + y}")
+# print(f" x - y = {x - y}")
+# print(f" x * y = {x * y}")
+# print(f" x / y = {x / y}")
+# print(f" x // y = {x // y}")
+# print(f" x ** y = {x ** y}")
+# print(f" x > y = {x > y}")
+# print(f" x < y = {x < y}")
+# print(f" x == y = {x == y}")
+# print(f" x != y = {x != y}")
+# print(f" x >= y = {x >= y}")
+# print(f" x <= y = {x <= y}")
+
+
+# n1 = 10
+# n2 = 20
+# n3 = 30
+
+# if n1 > n2 and n1 > n3:
+#     print('n1 is largest')
+# if n2 > n1 and n2 > n3:
+#     print('n2 is largest')
+# if n3 > n1 and n3 > n2:
+#     print('n3 is largest')
+
+
+# ch = input('Enter A Character')
+
+# if ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U' or ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+#     print('Entered Character Is Vowel')
+# else:
+#     print('Entered Character Is Not Vowel')
+    
+
+# x = 10
+# y = 6
+# list = [10, 20, 30, 40, 50]
+
+# print(x in list)
+# print(y in list)
+# print(y not in list)
+# print(x is y)
+# print(x is not y)
+
+
+
+# x = 20
+# y = 10
+
+# if x == y:
+#     print('Both Numbers Are Equal')
+# elif x > y:
+#     print('X is greater then Y')
+# else:
+#     print('X is less Then Y')
+
+
+# x = 30
+
+
+# if x >= 0:
+#     if x == 0:
+#         print(' X is 0')
+#     else:
+#         print('X is Positive Number')
+# else:
+#     print(' X is negative Number')
+
+
+# i=0
+# while i < 5:
+#     print(i)
+#     i = i + 1
+
+
+
+# for i in 'Hello':
+#     print(i, end=' ')
+
+# print()
+
+# list1 = [10, 20, 'Akash', 'Technolabs']
+# for i in list1:
+#     print(i, end=' ')
+
+# print()
+
+# for i in range(5):
+#     print(i, end=' ')
+
+# print()
+
+# for i in range(3, 5):
+#     print(i, end=' ')
+
+# print()
+
+# for i in range(1, 5, 2):
+#     print(i, end=' ')
+
+
+
+list1 = [10, 10.5, 'Akash', 'Technolabs']
+for i in range(len(list1)):
+    print(list1[i])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
