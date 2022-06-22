78f06e911b4383b9be40c8521f18b6902958c860
Mihir Davada <101976200+MihirDavada@users.noreply.github.com>
2022-06-22 20:56:15 +0530

    Add files via upload

---
+++ DAY1/Code/day1.py
@@ -0,0 +1,5 @@
+# Name: Mihir Davada
+# College : L.D.College Of Engineering
+# role : pythonDjango
+
+print('Hello World')
---
+++ DAY2/Code/day2.py
@@ -0,0 +1,120 @@
+# Name: Mihir Davada
+# College : L.D.College Of Engineering
+# role : pythonDjango
+
+'''
+This Is Multiline Comment
+'''
+
+# This Is Single Line Comment
+
+# a = 10
+# b = 10.5
+# c = 'Akash Infolabs'
+
+# print(f" The Value Of A is { a } ")
+# print(f" The Value Of B is { b } ")
+# print(f" The Value Of C is { c } ")
+
+# name = 'Akash Technolabs'
+# print(f" Name is {name}")
+
+# name = 'akashsir.com'
+# print(f" Name is {name}")
+
+
+# a, b, c = 10, 10.5, 'Akash Technolabs'
+
+# print(f" The Value Of A is { a } ")
+# print(f" The Value Of B is { b } ")
+# print(f" The Value Of C is { c } ")
+
+
+# a = b = c = 10
+
+# print(f" The Value Of A is { a } ")
+# print(f" The Value Of B is { b } ")
+# print(f" The Value Of C is { c } ")
+
+# n1 = 10
+# print(f"{n1} is type of {type(n1)}")
+
+# n2 = 10.5
+# print(f"{n2} is type of {type(n2)}")
+# print(f"{n2} is Complex Number? {isinstance(n2, int)}")
+
+# n3 = 1 + 2j
+# print(f"{n3} is type of {type(n3)}")
+# print(f"{n3} is Complex Number? {isinstance(n3, complex)}")
+
+# name = 'Aakash Technolabs'
+
+# print(f"name is {name}")
+
+# Print First Character Of String
+# print(name[0])
+
+# Print 3rd to 5 character 
+# print(name[2:5])
+
+# Print string which starts from third character
+# print(name[2:])
+
+# Print name 2 times
+# print(name * 2)
+
+# Concation Of String
+# print( name + ' Hello' )
+
+
+# list1 = [ 10, 20, 30, 'Akash', 40, 50, 'Technolabs' , 60]
+
+# print(f"List1 is {list1}")
+# print(f" Type Of List1 is { type(list1)}")
+
+# list1[2] = 30
+# print(f" list1[2] = {list1[2]} ")
+
+# list1[0:3] = [10, 20, 30]
+# print(f" list1[0:3] = {list1[0:3]} ")
+
+# list1[5:] = [50, 'Technolabs', 60]
+# print(f" list1[5:] = {list1[5:]} ")
+
+
+# list = []
+
+# n = int(input('Enter Size Of List'))
+
+# for i in range(n):
+#     val = input('Enter Value')
+
+#     list.append(val)
+
+# print(list)
+
+
+# tuple1 = (10, 20, 30, 'Akash', 40, 50, 'Technolabs' , 60)
+
+# print(f"tuple1 is {tuple1}")
+# print(f" Type Of tuple1 is { type(tuple1)}")
+
+# # tuple1[2] = 30
+# print(f" tuple1[2] = {tuple1[2]} ")
+
+# # tuple1[0:3] = [10, 20, 30]
+# print(f" tuple1[0:3] = {tuple1[0:3]} ")
+
+# # tuple1[5:] = [50, 'Technolabs', 60]
+# print(f" tuple1[5:] = {tuple1[5:]} ")
+
+
+d1 = {1:'Aakash', 2:'Technolabs', 'key':10}
+
+print(f" type of d1 is {d1}")
+
+print(f" d1[1] = {d1[1]} " )
+
+print(f" d1[2] = {d1[2]} " )
+
+print(f" d1['key'] = {d1['key']} ")
