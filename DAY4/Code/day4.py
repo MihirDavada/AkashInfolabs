7dc731ca49644c1d892e812508c44f78d98e5869
Mihir Davada <101976200+MihirDavada@users.noreply.github.com>
2022-07-02 18:40:40 +0530

    Add files via upload

---
+++ DAY4/Code/day4.py
@@ -0,0 +1,18 @@
+# list1 = [10, 10.5, 'Akash', 'Technolabs']
+# for i in range(len(list1)):
+#     print(f" Value Is {list1[i]}")
+# else:
+#     print('No Element Left')
+
+
+# i = 0
+# while i < 10:
+#     print(f" value is {i}")
+#     i = i+1
+#     if i >= 5:
+#         break
+
+for x in range(10):
+    if x % 2 == 0:
+        continue
+    print(f" The Value Of X is {x}")
