e53e5bb5d844138b8080a753cc118b58a9e15498
Mihir Davada <101976200+MihirDavada@users.noreply.github.com>
2022-07-02 18:41:13 +0530

    Add files via upload

---
+++ DAY5/Code/day5.py
@@ -0,0 +1,74 @@
+# Day5 Task - Mihir Davada
+
+# def show():
+#     n1=10
+#     n2=20
+#     ans=n1+n2
+#     print("Sum is ",ans)
+
+# show()
+
+# def show(n1,n2):
+#     ans=n1+n2
+#     return  ans
+
+# ans=show(10,20)
+# print("Sum is ",ans)
+
+
+# def show(n1,n2):
+#     ans=n1+n2
+#     print("Sum is ", ans)
+
+# show(10,20)
+
+
+
+# def show():
+#     n1=10
+#     n2=20
+#     ans=n1+n2
+#     return ans
+
+# ans=show()
+# print("Sum is ", ans)
+
+# Default Argument
+# def sum(a = 5, b = 6):
+#     print(a+b)
+# sum()
+# sum(10, 20)
+
+
+# def add(a, b):
+#     print(a+b)
+
+# add(b=10, a=20)
+
+
+
+# def add(*num):
+#     sum = 0
+
+#     for i in num:
+#         sum = sum + i
+
+#     print(f" Sum is {sum}")
+
+# add(10, 20, 30)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
